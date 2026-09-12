3e686c5562cc59d50510b066b601e7d2340e93bc
jaysinh thakor <jaysinhthakor6064@gmail.com>
2026-09-12 11:53:26 +0530

    Fix fraud engine RDS connection

--- fraud-engine/app/database.py
+++ fraud-engine/app/database.py
@@ -1,15 +1,13 @@
 import mysql.connector
 
-
 def get_db_connection():
     return mysql.connector.connect(
-        host="fraud-mysql",
+        host="fraud-mysql.cfey2gic407o.ap-south-1.rds.amazonaws.com",
         port=3306,
         user="fraud_user",
-        password="fraud_password",
+        password="Jaysinh6094",
         database="fraud_db"
     )
-
 
 def update_transaction_status(transaction_id: str, status: str):
     connection = get_db_connection()
